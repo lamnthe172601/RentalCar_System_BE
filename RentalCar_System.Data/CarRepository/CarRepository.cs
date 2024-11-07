f43ee33c55bebba3cb96c153f5ef710dc8fa4e0d
TranBaBinh <fanjfla1989@gmail.com>
2024-11-08 00:09:09 +0700

    Car

---
+++ RentalCar_System.Data/CarRepository/CarRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalCar_System.Data.CarRepository
+{
+    public class CarRepository : ICarRepository
+    {
+
+    }
+}
