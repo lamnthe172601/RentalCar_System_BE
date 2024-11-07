5623515d93683e53978934835d5aa155170f5312
TungLam_1286 <lamnt140303@gmail.com>
2024-11-08 01:31:36 +0700

    api update profile, change-pas

---
+++ RentalCar_System.Data/UserRepository/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using RentalCar_System.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalCar_System.Data.UserRepository
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(RentalCarDBContext context) : base(context)
+        {
+
+            
+        }
+    }
+}
---
+++ RentalCar_System.Models/DtoViewModel/ChangePasswordViewModel.cs
@@ -0,0 +1,18 @@
+﻿using RentalCar_System.Models.Validations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalCar_System.Models.DtoViewModel
+{
+    public class ChangePasswordViewModel {
+        
+        [Required(ErrorMessage = "Password is required")]
+        public string OldPassword { get; set; }
+
+        [PasswordValidator]
+        public string NewPassword { get; set; } }
+}
