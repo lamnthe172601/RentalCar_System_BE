4b2abc9054c20840bcce1f2dd4aa0fe2d49e2063
TranBaBinh <fanjfla1989@gmail.com>
2024-11-18 14:42:19 +0700

    add cart

---
+++ RentalCar_System.Data/CartRepository/ICartRepository.cs
@@ -0,0 +1,19 @@
+﻿using RentalCar_System.Models.DtoViewModel;
+using RentalCar_System.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalCar_System.Data.CartRepository
+{
+    public interface ICartRepository
+    {
+        Task<IEnumerable<Cart>> GetCartItemsByUserIdAsync(Guid userId);
+        Task<Cart> GetCartItemByIdAndUserIdAsync(Guid cartId, Guid userId);
+        Task AddCartItemAsync(Cart cartItem);
+        Task RemoveCartItemAsync(Cart cartItem);
+        Task SaveChangesAsync();
+    }
+}
