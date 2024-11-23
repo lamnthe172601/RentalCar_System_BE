b39d9e04c0e25ff395ae102a7f02131f424abf7f
XuanHung <xuanhungdz3011@gmail.com>
2024-11-24 02:02:34 +0700

    Create IPaymentRepository.cs

---
+++ RentalCar_System.Data/PaymentRepository/IPaymentRepository.cs
@@ -0,0 +1,11 @@
+﻿using RentalCar_System.Models.Entity;
+
+namespace RentalCar_System.Data.PaymentRepository
+{
+    public interface IPaymentRepository
+    {
+        Task<decimal> GetTotalAmountAsync();
+        Task<int> GetPaymentCountByStatusAsync(string status);
+        Task<IEnumerable<Payment>> GetPaymentsByDateRangeAsync(DateTime startDate, DateTime endDate);
+    }
+}
